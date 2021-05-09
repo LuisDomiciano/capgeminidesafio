89fab65a4afa8f5ac703ac221fd2d7f74708554d
LuisD <luis.domiciano@hotmail.com>
2021-05-08 22:45:04 -0300

    feat: advertisements route

--- src/controller/advertisementController.js
+++ src/controller/advertisementController.js
@@ -1,3 +1,14 @@
 exports.index = (request, response) => {
-  return response.status(200).send({ message: 'Obtendo dados' })
+  return response.status(200).send({
+    id: 1,
+    name: 'Capgemine Programa de Formação',
+    client: 'Capgemine',
+    dateStart: '22/04/2021',
+    dataExpirated: '22/05/2021',
+    investiment: 2.500
+  })
+}
+
+exports.post = (request, response) => {
+  return response.status(201).json(request.body)
 }
